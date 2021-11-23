9067de9b752bd61699daabe9669bde46a318b560
Ethan He <yfhe@ucdavis.edu>
2021-11-23 14:43:42 -0800

    add reverse

--- part1.pl
+++ part1.pl
@@ -1,11 +1,19 @@
 % concat(L1, L2, L)
 % L is the concatenation of L1 and L2
 concat([], [], []).
-concat([], [Y | YS], [Y | ZS]) :- concat([], YS, ZS).
-concat([X | XS], YS, [X | ZS]) :- concat(XS, YS, ZS).
+concat([], [Y | Ys], [Y | Zs]) :- concat([], Ys, Zs).
+concat([X | Xs], Ys, [X | Zs]) :- concat(Xs, Ys, Zs).
 
 % element_at(X, N, L)
 % element X is the Nth element in list L
 element_at(X, 1, [X | _]).
-element_at(X, N2, [Y | YS]) :- element_at(X, N1, YS), N2 is N1 + 1.
+element_at(X, N2, [_ | Ys]) :- element_at(X, N1, Ys), N2 is N1 + 1.
+
+% myReverse(L1, L2) the reverse of list L1 is L2
+reverseHelper([], Xs, Xs).
+reverseHelper([X | Xs], Ys, Zs) :- reverseHelper(Xs, Ys, [X | Zs]).
+
+myReverse(Xs, Ys) :- reverseHelper(Xs, Ys, []).
+
+
 
