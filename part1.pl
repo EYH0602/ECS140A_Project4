cb45aeabd327930a4024bc4a50a067b5714d05ef
Ethan He <yfhe@ucdavis.edu>
2021-11-23 17:28:53 -0800

    add compress

--- part1.pl
+++ part1.pl
@@ -10,10 +10,11 @@ element_at(X, 1, [X | _]).
 element_at(X, N2, [_ | Ys]) :- element_at(X, N1, Ys), N2 is N1 + 1.
 
 % myReverse(L1, L2) the reverse of list L1 is L2
+myReverse(Xs, Ys) :- reverseHelper(Xs, Ys, []).
+
 reverseHelper([], Xs, Xs).
 reverseHelper([X | Xs], Ys, Zs) :- reverseHelper(Xs, Ys, [X | Zs]).
 
-myReverse(Xs, Ys) :- reverseHelper(Xs, Ys, []).
 
 % flatten(L1, L2)
 % L2 is the flattened version of L1
@@ -24,4 +25,11 @@ myFlatten([X | Xs], Ys) :-
     myFlatten(Xs, FlattenXs), 
     concat(FlattenX, FlattenXs, Ys).
 
+% compress(L1, L2)
+% L1, L2 is its compressed version by eliminating the duplicates.
+compress(X, X).
+compress(Xs, Ys) :- compressHelper(Xs, Ys, []).
 
+compressHelper([], Ys, Zs) :- reverse(Ys, Zs).
+compressHelper([X | Xs], Ys, [X | Zs]) :- compressHelper(Xs, Ys, [X | Zs]).
+compressHelper([X | Xs], Ys, Zs) :- compressHelper(Xs, Ys, [X | Zs]).
